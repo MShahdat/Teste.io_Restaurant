4a7fdf80a7933a62b4ca6fd9a96b8350695f8ca6
Md Shahdat Hossain <mdshahdat2504@gmail.com>
2026-01-23 01:31:11 +0600

    final updated

--- src/firebase/Firebase.jsx
+++ src/firebase/Firebase.jsx
@@ -4,14 +4,7 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAsBsaG3IOvzEOCIMXVG0zhLFGi2nmqLHw",
-  authDomain: "resturent-30be2.firebaseapp.com",
-  projectId: "resturent-30be2",
-  storageBucket: "resturent-30be2.firebasestorage.app",
-  messagingSenderId: "1078847575594",
-  appId: "1:1078847575594:web:05a039b0f7bbc16b819553"
-};
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
